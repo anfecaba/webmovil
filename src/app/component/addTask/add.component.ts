23979c9c29a6ecdc0f6e6548b234e93df29f9663
anfecaba <acamacho438@gmail.com>
2026-04-21 09:17:27 -0500

    feat: add task management components and directives
    - Implemented AddComponent for adding tasks with validation.
    - Created ListTask component for displaying tasks.
    - Added StatusTask directive for task completion styling.
    - Introduced StatusDelete directive for confirming task deletion.
    - Defined Task interface for task structure.
    - Updated app component to include new components.
    - Added styles for task components and global styles.

---
+++ src/app/component/addTask/add.component.ts
@@ -0,0 +1,100 @@
+import { Component, inject, OnInit } from "@angular/core";
+import { ReactiveFormsModule, FormGroup, FormBuilder, FormControl, Validators, NgForm } from "@angular/forms";
+import { NgClass, NgStyle } from "@angular/common";
+import { Task } from "../../models/task.interface";
+import { StatusTask } from "../../directives/status-task.directive";
+import { StatusDelete } from "../../directives/status-delete.directive";
+
+
+
+@Component({
+
+    selector: "app-add-task", 
+    styleUrl: './add.component.scss',
+    templateUrl: './add.component.html',
+    imports: [ReactiveFormsModule, NgClass, NgStyle, StatusTask, StatusDelete],
+    standalone : true
+    
+
+
+})
+
+export class AddComponent implements OnInit{ 
+
+    numbTask!: number;
+    titleTask = ""
+    activeButton = true
+    isActive = true
+    tasks: Task[] = [
+        {
+        id: 1,
+        title: "Tarea 1",
+        completed: false},
+            {
+                id: 2,
+                title: "Tarea 2",
+                completed: false},
+                {
+                    id: 3,
+                    title: "Tarea 3",
+                    completed: false}
+              
+    ]
+
+    fb = inject(FormBuilder);
+
+    form! :FormGroup;
+
+
+    ngOnInit(): void {
+        this.form = this.fb.group({
+            title: new FormControl("", [Validators.required, Validators.maxLength(8)])
+        })
+    }
+
+    sendTitleTask(){
+        if(this.form.valid){
+            console.log(this.form.value.title)
+        }
+    }
+
+    markTaskCompleted(task: Task, event: Event) {
+        const input = event.target as HTMLInputElement;
+        task.completed = input.checked;
+    
+    }
+
+
+    delete(id: number){
+        this.tasks = this.tasks.filter((task)=> task.id !==id)
+        this.numbTask = this.tasks.length;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    sendData(form: NgForm){
+        if(form.valid){
+            console.log("Tarea enviada ${titleTask}")
+        }
+
+    }
+
+    sendTask(){
+        const arrayTitleTask = this.titleTask.split("")
+            if(arrayTitleTask.length >0){
+                this.activeButton= true
+            }else{
+                this.activeButton = false
+            }
+        }
+    }
